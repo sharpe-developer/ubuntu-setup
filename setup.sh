adbac71e2c544c810c1de9f1e5dbd6ed0a2ed7a3
sharpe <sharpe.developer@gmail.com>
2020-04-16 10:43:00 -0700

    Add desktop background setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+# Set desktop background to solid color
+gsettings set org.gnome.desktop.background picture-uri ''
+gsettings set org.gnome.desktop.background primary-color 'rgb(66, 81, 100)'
+
+# Set terminal color profile
 dconf load /org/gnome/terminal/ < gnome_terminal_settings_backup.txt
 
+# Add new file templates
 cp -a ./templates/. ~/Templates
 chmod +x ~/Templates/*.py
 chmod +x ~/Templates/*.sh
